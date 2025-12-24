3b5ac872a6b29a61aeaf99d53aa1e8296506743e
vjvinayakam <117278487+vjvinayakam@users.noreply.github.com>
2025-12-24 11:42:07 +0530

    Delete queries directory

--- queries/01_total_users_events.sql
+++
@@ -1,5 +0,0 @@
--- Total users and total events
-SELECT
-  COUNT(DISTINCT user_pseudo_id) AS total_users,
-  COUNT(*) AS total_events
-FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`;
